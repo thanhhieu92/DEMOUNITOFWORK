10ed935867fd34cb899e45e474a71e35948a59d3
thanhhieu92 <hieu64822@gmail.com>
2023-03-27 19:09:25 +0700

    complete interface repository

--- DEMOUNITOFWORK/Repository/IOrderRepository.cs
+++ DEMOUNITOFWORK/Repository/IOrderRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace DEMOUNITOFWORK.Repository
+﻿using DEMOUNITOFWORK.Models;
+
+namespace DEMOUNITOFWORK.Repository
 {
     public interface IOrderRepository
     {
+        List<Order> GetOrders();
+        Order OrderGetOrder(int id);
+        void AddOrder(Order order);
+        void UpdateOrder(Order order);
+        void DeleteOrder(int id);
     }
 }
--- DEMOUNITOFWORK/Repository/IProductRepository.cs
+++ DEMOUNITOFWORK/Repository/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace DEMOUNITOFWORK.Repository
+﻿using DEMOUNITOFWORK.Models;
+
+namespace DEMOUNITOFWORK.Repository
 {
     public interface IProductRepository
     {
+        List<Order> GetProducts();
+        Order GetProduct(int id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
     }
 }
